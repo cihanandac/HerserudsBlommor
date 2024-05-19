fb1464ff285ffe64ea5aa0b435302680f1dfc081
Cihan Andac <cihanandac@gmail.com>
2024-05-19 11:46:12 +0200

    chores

--- src/components/Specialdays.tsx
+++ src/components/Specialdays.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 import config from '../config/index.json';
 
 const Specialdays = () => {
-  // const { pricing } = config;
-  // const { items, title } = pricing;
-  // const [firstPlan, secondPlan, thirdPlan] = items;
   const { specialdays } = config;
   const [firstItem, secondItem, thirdItem, fourthItem] = specialdays.items;
 
@@ -18,16 +15,16 @@ const Specialdays = () => {
         <div className="w-full mb-4">
           <div className="h-1 mx-auto bg-primary w-64 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
-        <div className="flex flex-col sm:flex-row justify-center items-center pt-12 my-12 sm:my-4 space-y-8 sm:space-y-0 sm:space-x-8">
+        <div className="flex flex-col sm:flex-row justify-center items-stretch pt-12 my-12 sm:my-4 space-y-8 sm:space-y-0 sm:space-x-8">
           {/* <!-- Brudbuketter Section --> */}
-          <div className="flex flex-col items-center text-center w-full sm:w-1/3">
+          <div className="flex flex-col h-full items-center text-center w-full sm:w-1/3 flex-grow">
             <h3 className="text-xl text-secondary font-bold mb-2 font-sofiasans">
               {firstItem?.title}
             </h3>
             <p className="mb-4 text-secondary font-sofiasans">
               {firstItem?.description}
             </p>
-            <div className="w-full p-6">
+            <div className="w-full p-6 flex-grow">
               <img
                 className="w-[250px] aspect-square rounded-full object-cover mx-auto"
                 src={firstItem?.img}
@@ -36,14 +33,14 @@ const Specialdays = () => {
             </div>
           </div>
           {/* <!-- Event Dekoration Section --> */}
-          <div className="flex flex-col items-center text-center w-full sm:w-1/3">
+          <div className="flex flex-col h-full items-center text-center w-full sm:w-1/3 flex-grow">
             <h3 className="text-xl text-secondary font-bold mb-2 font-sofiasans">
               {secondItem?.title}
             </h3>
             <p className="mb-4 text-secondary font-sofiasans">
               {secondItem?.description}
             </p>
-            <div className="w-full p-6">
+            <div className="w-full p-6 flex-grow">
               <img
                 className="w-[250px] aspect-square rounded-full object-cover mx-auto"
                 src={secondItem?.img}
@@ -52,14 +49,14 @@ const Specialdays = () => {
             </div>
           </div>
           {/* <!-- Nyblivna Föräldrar Section --> */}
-          <div className="flex flex-col items-center text-center w-full sm:w-1/3">
+          <div className="flex flex-col h-full items-center text-center w-full sm:w-1/3 flex-grow">
             <h3 className="text-xl font-bold mb-2 text-secondary font-sofiasans">
               {thirdItem?.title}
             </h3>
             <p className="mb-4 text-secondary font-sofiasans">
               {thirdItem?.description}
             </p>
-            <div className="w-full p-6">
+            <div className="w-full p-6 flex-grow">
               <img
                 className="w-[250px] aspect-square rounded-full object-cover mx-auto"
                 src={thirdItem?.img}
@@ -67,14 +64,14 @@ const Specialdays = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col justify-start items-center text-center w-full sm:w-1/3">
+          <div className="flex flex-col h-full items-center text-center w-full sm:w-1/3 flex-grow">
             <h3 className="text-xl font-bold mb-2 text-secondary font-sofiasans">
               {fourthItem?.title}
             </h3>
             <p className="mb-4 text-secondary font-sofiasans">
               {fourthItem?.description}
             </p>
-            <div className="w-full p-6">
+            <div className="w-full p-6 flex-grow">
               <img
                 className="w-[250px] aspect-square rounded-full object-cover mx-auto"
                 src={fourthItem?.img}
